b27ad068f00a15e06345aee859085914120e9426
Pattie01 <48245170+Pattie01@users.noreply.github.com>
2019-06-03 18:02:57 +0200

    Update zadanie5.cpp

--- Labolatorium 11/zadanie5.cpp
+++ Labolatorium 11/zadanie5.cpp
@@ -19,7 +19,7 @@ T Mean(vector<T> values){
 int main() {
     vector<int> int_vec{1,45,4,7,6};
     vector<double> double_vec{3.5,2.4,7.4,1.1};
-    vector <complex<int>> com_vec{(2,3),(4,1),(5,3)};
+    vector <complex<int>> com_vec{{2,3},{4,1},{5,3}};
 
     std::cout << Mean(int_vec) << std::endl;
     std::cout << Mean(double_vec) << std::endl;
